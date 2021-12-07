0d69f22712617bf79c67511bfca0f1cabb40260e
Nathan Schmidt <nschmdt@gmail.com>
2021-12-07 15:57:17 -0600

    Update Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	Routes,
+	Route,
+	Navigate
+} from 'react-router-dom';
+
+import Header from "./components/Header";
+import Courses from './components/courses/Courses';
+import CourseDetail from './components/courses/CourseDetail';
+import UpdateCourse from './components/courses/UpdateCourse';
+import CreateCourse from './components/courses/CreateCourse';
+import NotFound from './components/Errors/NotFound';
+import Forbidden from './components/Errors/Forbidden';
+import UnhandledError from './components/Errors/UnhandledError';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+	  <div id="root">
+		<Header />
+		<main>
+		  <Routes>
+			<Route path="/" element={<Navigate replace to="/courses" />} />
+			<Route path="/courses" element={<Courses />} />
+			<Route path="/courses/:id" element={<CourseDetail />} />
+			<Route path="/courses/:id/update" element={<UpdateCourse />} />
+			<Route path='/courses/create' element={<CreateCourse />} />
 
-export default App;
+			<Route path="/notfound" element={<NotFound />} />
+			<Route path="/forbidden" element={<Forbidden />} />
+			<Route path="/error" element={<UnhandledError />} />
+			
+			<Route path="*" element={<NotFound />} />
+		  </Routes>
+		</main>
+	  </div>
+	);
+  }
+  
+  export default App;
