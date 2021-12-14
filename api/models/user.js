46822bdbefc0de3b5880c0b4b8db27e91be8a829
Nathan Schmidt <nschmdt@gmail.com>
2021-12-13 20:03:15 -0600

    Add id to user model

--- api/models/user.js
+++ api/models/user.js
@@ -5,6 +5,11 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize) => {
     class User extends Model {}
     User.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         firstName: {
             type: DataTypes.STRING,
             allowNull: false,
