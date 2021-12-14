e2527de096e4294e342483be9ae7e4cbabafdd77
Nathan Schmidt <nschmdt@gmail.com>
2021-12-13 22:06:33 -0600

    Update course detail

--- client/src/components/courses/CourseDetail.js
+++ client/src/components/courses/CourseDetail.js
@@ -1,85 +1,117 @@
-import React, { useEffect, useContext, useState } from 'react'
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import Context from '../../Context';
-import ReactMarkdown from 'react-markdown';
-import Loading from '../Loading';
+import React, { useEffect, useContext, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import Context from "../../Context";
+import ReactMarkdown from "react-markdown";
+import Loading from "../Loading";
 
 function CourseDetail() {
-    const context = useContext(Context.Context);
-    const authUser = context.authenticatedUser;
-    const { id } = useParams();
-    let navigate = useNavigate();
-    let [courseDetail, setCourseDetail] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+	const context = useContext(Context.Context);
+	const authUser = context.authenticatedUser;
+	const { id } = useParams();
+	let navigate = useNavigate();
+	let [courseDetail, setCourseDetail] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const controller = new AbortController();
-        if(!/^\d+$/.test(id)){
-            console.log("Not a Number id");
-            navigate('/notfound');
-        }
+	useEffect(() => {
+		const controller = new AbortController();
+		if (!/^\d+$/.test(id)) {
+			console.log("Not a Number id");
+			navigate("/notfound");
+		}
 
-        context.data.getCourse(id)
-        .then(response => {
-            setCourseDetail(response);
-            setIsLoading(false);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+		context.data
+			.getCourse(id)
+			.then((response) => {
+				setCourseDetail(response);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
-        return () => {
-            controller.abort();
-        }
-    }, [id, navigate, context]);
+		return () => {
+			controller.abort();
+		};
+	}, [id, navigate, context]);
 
-    const handleDelete = () => {
+	const handleDelete = () => {
+		context.data
+			.deleteCourse(id, authUser.emailaddress, authUser.password)
+			.then((response) => {
+				if (response.length) {
+					navigate("/error");
+				} else {
+					navigate("/");
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				navigate("/error");
+			});
+	};
 
-    }
+	return isLoading ? (
+		<Loading />
+	) : (
+		<>
+			<div className="actions--bar">
+				<div className="wrap">
+					{authUser && authUser.id === courseDetail.userId ? (
+						<>
+							<Link
+								to={`/courses/${id}/update`}
+								className="button"
+							>
+								Update Course
+							</Link>
+							<button className="button" onClick={handleDelete}>
+								Delete Course
+							</button>
+						</>
+					) : null}
+					<Link className="button button-secondary" to="/">
+						Return to List
+					</Link>
+				</div>
+			</div>
 
-    return (
-        isLoading ?
-            <Loading />
-        :
-            <>
-                <div className="actions--bar">
-                    <div className="wrap">
-                        {
-                            authUser && (authUser.id === courseDetail.userId) ?
-                            <>
-                                <Link to={`/courses/${id}/update`} className="button">Update Course</Link>
-                                <button className="button" onClick={handleDelete}>Delete Course</button>
-                            </>
-                            : null
-                        }
-                        <Link className="button button-secondary" to="/">Return to List</Link> 
-                    </div>
-                </div>
-                
-                <div className="wrap">
-                    <h2>Course Detail</h2>
-                    <form>
-                        <div className="main--flex">
-                            <div>
-                                <h3 className="course--detail--title">Course</h3>
-                                <h4 className="course--name">{courseDetail.title}</h4>
-                                <p>By: {courseDetail.user.firstName} {courseDetail.user.lastName}</p>
-                                <p>{courseDetail.description}</p>
-                            </div>
-                            <div>
-                                <h3 className="course--detail--title">Estimated Time</h3>
-                                <ReactMarkdown>{courseDetail.estimatedTime}</ReactMarkdown>
+			<div className="wrap">
+				<h2>Course Detail</h2>
+				<form>
+					<div className="main--flex">
+						<div>
+							<h3 className="course--detail--title">Course</h3>
+							<h4 className="course--name">
+								{courseDetail.title}
+							</h4>
+							<p>
+								By: {courseDetail.user.firstName}{" "}
+								{courseDetail.user.lastName}
+							</p>
+							<p>{courseDetail.description}</p>
+						</div>
+						<div>
+							<h3 className="course--detail--title">
+								Estimated Time
+							</h3>
+							<ReactMarkdown>
+								{courseDetail.estimatedTime}
+							</ReactMarkdown>
 
-                                <h3 className="course--detail--title">Materials Needed</h3>
-                                <ul className="course--detail--list">
-                                    <ReactMarkdown>{courseDetail.materialsNeeded}</ReactMarkdown>
-                                </ul>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </>
-    )
-}   
+							<h3 className="course--detail--title">
+								Materials Needed
+							</h3>
+							<ul className="course--detail--list">
+								<ReactMarkdown>
+									{courseDetail.materialsNeeded}
+								</ReactMarkdown>
+							</ul>
+						</div>
+					</div>
+				</form>
+			</div>
+		</>
+	);
+}
 
 export default CourseDetail;
