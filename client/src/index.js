54acd4478d478f9b7fa73ec10d1494d36747614f
Nathan Schmidt <nschmdt@gmail.com>
2021-12-13 20:07:01 -0600

    Add Provider Context

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-//import { Provider } from './Context';
+import { Provider } from './Context';
 
 import './styles/reset.css';
 import './styles/global.css';
 
 ReactDOM.render(
-  //<Provider>
+  <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>,
-  //</Provider>,
+  </Provider>,
   document.getElementById('root')
 );
 
