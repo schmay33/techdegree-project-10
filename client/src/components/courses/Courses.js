c7cb700d4737ff739b7a0d52360cf396097a42ed
Nathan Schmidt <nschmdt@gmail.com>
2021-12-13 20:08:10 -0600

    Add main courses layout

--- client/src/components/courses/Courses.js
+++ client/src/components/courses/Courses.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Context from '../../Context';
+import Loading from '../Loading';
 
 
-function Courses() {
-    return(
-        <div className="wrap main--grid">
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Build a Basic Bookcase</h3>
-            </a>
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Learn How to Program</h3>
-            </a>
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Learn How to Test Programs</h3>
-            </a>
-            <a className="course--module course--add--module" href="create-course.html">
+const Courses = () => {
+    const context = useContext(Context.Context);
+    const [data, setData] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    let [courses] = useState('');
+    let navigate = useNavigate();
+  
+    useEffect(() => {
+      context.data.getCourses()
+        .then((response) => {
+          setData(response);
+        })
+        .catch((error) => {
+          console.error('Error fetching and parsing data from courses api', error);
+          navigate('/error');
+        })
+        .finally(() => setIsLoading(false));
+    }, [navigate, context.data]);
+  
+    if (data.length) {
+      courses = data.map((course) => {
+        return <Link to={`/courses/${course.id}`} className="course--module course--link" key={course.id}>
+          <h2 className="course--label">Course</h2>
+          <h3 className="course--title">{course.title}</h3>
+        </Link>
+      });
+    }
+  
+    return (
+        isLoading ?
+            <Loading />
+        : <div className="wrap main--grid">
+            {courses}
+            <Link to='/courses/create' className="course--module course--add--module">
                 <span className="course--add--title">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                    viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                    New Course
+                        viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                        New Course
                 </span>
-            </a>
+            </Link>
         </div>
     );
-}
-
-export default Courses;
+  }
+  
+  export default Courses;
