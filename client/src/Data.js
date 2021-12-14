78e4330869181119746f72073a1301970f61321b
Nathan Schmidt <nschmdt@gmail.com>
2021-12-13 20:06:52 -0600

    Add new user and course api calls

--- client/src/Data.js
+++ client/src/Data.js
@@ -19,6 +19,7 @@ export default class Data {
 		};
 
 		if (body !== null) {
+            //console.log(body);
 			options.body = JSON.stringify(body);
 		}
 
@@ -28,6 +29,67 @@ export default class Data {
 			);
 			options.headers["Authorization"] = `Basic ${encodedCredentials}`;
 		}
+        console.log(options);
 		return fetch(url, options);
 	}
+
+    //TODO: Get User
+    async getUser(username, password) {
+        const response = await this.api('/users', 'GET', null, true, {username, password});
+        if (response.status === 200) {
+            return response.json().then(data => data);
+        }
+    }
+
+    //TODO: Create User
+    async createUser(user) {
+        const response = await this.api('/users', 'POST', user, false, null);
+        if (response.status === 201) {
+            return [];
+        }
+        else if (response.status === 400) {
+            return response.json().then(data => {
+              return data.errors;
+            });
+        }
+        else {
+            throw new Error();
+        }
+    }
+
+    // Get Courses
+    async getCourses() {
+        const response = await this.api('/courses', 'GET', null, false);
+        if (response.status === 200) {
+          return response.json().then(data => data);
+        } else {
+          throw new Error();
+        }
+    }
+
+    //TODO: Get course by id
+    async getCourse(id) {
+        const response = await this.api(`/courses/${id}`, 'GET', null, false);
+        if (response.status === 200) {
+            return response.json().then(data => data);
+          } else {
+            throw new Error();
+        }
+    }
+
+    // update course
+    async updateCourse(id, course, username, password) {
+        const response = await this.api(`/courses/${id}`, 'PUT', course, true, { username, password });
+        if (response.status === 204) {
+          return [];
+        }
+        else if (response.status === 400) {
+          return response.json().then(data => {
+            return data.errors;
+          });
+        }
+        else {
+          throw new Error();
+        }
+      }
 }
