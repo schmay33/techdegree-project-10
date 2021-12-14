3eec1903729c2167dd5517544cd168c8e4761d7b
Nathan Schmidt <nschmdt@gmail.com>
2021-12-14 11:04:37 -0600

    Update validation message

--- api/models/user.js
+++ api/models/user.js
@@ -1,87 +1,93 @@
-'use strict';
-const {Model, DataTypes} = require('sequelize');
-const bcrypt = require('bcryptjs');
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize) => {
-    class User extends Model {}
-    User.init({
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: 'A First Name is Required'
+	class User extends Model {}
+	User.init(
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			firstName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "A First Name is Required",
+					},
+					notEmpty: {
+						msg: "Please provide a First Name",
+					},
+				},
+			},
+			lastName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "A Last Name is Required",
+					},
+					notEmpty: {
+						msg: "Please provide a Last Name",
+					},
+				},
+			},
+			emailAddress: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: {
+                  msg: 'The email address you entered already exists.'
                 },
-                notEmpty: {
-                    msg: 'Please provide a first name'
-                },
-            }
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: 'A Last Name is Required'
-                },
-                notEmpty: {
-                    msg: 'Please provide a Last Name'
-                },
-            }
-        },
-        emailAddress: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: {
-                msg: 'The email address you entered already exists.',
-            },
-            validate: {
-                isEmail: {
-                    args: true,
-                    msg: 'Please use a valid email format: xxx@email.com'
-                },
-                notNull: {
+                validate: {
+                  notNull: {
                     msg: 'An email address is required'
-                },
-                notEmpty: {
-                    msg: 'Please provide an email address'
+                  },
+                  notEmpty: {
+                    msg: 'Please provide an email address.'
+                  },
+                  isEmail: {
+                    msg: 'Please enter in a valid email address.'
+                  }
                 }
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                min: 8,
-                max: 20,
-                notNull: {
-                    msg: 'A password is required'
-                },
-                notEmpty: {
-                    msg: 'Please provide a password'
-                }
-            },
-            set(val) {
-                const hashedPassword = bcrypt.hashSync(val);
-                this.setDataValue('password', hashedPassword);
-            },
-        }
-    }, { sequelize });
+              },
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "A password is required",
+					},
+					notEmpty: {
+						msg: "Please provide a password.",
+					},
+					validatePassword(val) {
+						if (val.length >= 8 && val.length <= 20) {
+							const hashedPassword = bcrypt.hashSync(val, 10);
+							this.setDataValue("password", hashedPassword);
+						} else {
+							throw new Error(
+								"Your password should be between 8 and 20 characters"
+							);
+						}
+					},
+				},
+			},
+		},
+		{ sequelize }
+	);
 
-    User.associate = (models) => {
-        User.hasMany(models.Course, {
-            as: 'user',
-            foreignKey: {
-                fieldName: 'userId',
-                allowNull: false
-            }
-        });
-    };
+	User.associate = (models) => {
+		User.hasMany(models.Course, {
+			as: "user",
+			foreignKey: {
+				fieldName: "userId",
+				allowNull: false,
+			},
+		});
+	};
 
-    return User;
-}
+	return User;
+};
